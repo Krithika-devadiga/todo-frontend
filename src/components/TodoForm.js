3b9d917c5718a30d1071fa823e277b77d79c8326
Krithika-devadiga <krithikadevadiga13@gmail.com>
2025-06-21 12:26:43 +0530

    Initial commit

---
+++ src/components/TodoForm.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import api from '../api';
+import '../App.css';
+
+function TodoForm({ onTodoAdded }) {
+ const [title, setTitle] = useState('');
+
+ const handleSubmit = async (e) => {
+   e.preventDefault();
+   await api.post('/', { title });
+   onTodoAdded();
+   setTitle('');
+ };
+
+ return (
+  
+   <form onSubmit={handleSubmit}>
+     <input
+       placeholder="Add a task"
+       value={title}
+       onChange={e => setTitle(e.target.value)}
+       required
+     />
+     <button type="submit">Add</button>
+   </form>
+  
+ );
+}
+
+export default TodoForm;
---
+++ src/components/TodoList.js
@@ -0,0 +1,21 @@
+import '../App.css';
+
+function TodoList({ todos, onDelete, onToggle }) {
+ return (
+   <ul>
+     {todos.map(todo => (
+       <li key={todo._id}>
+         <span
+           style={{ textDecoration: todo.completed ? 'line-through' : 'none', cursor: 'pointer' }}
+           onClick={() => onToggle(todo._id, !todo.completed)}
+         >
+           {todo.title}
+         </span><br></br>
+         <button onClick={() => onDelete(todo._id)}>Delete</button>
+       </li>
+     ))}
+   </ul>
+ );
+}
+
+export default TodoList;
