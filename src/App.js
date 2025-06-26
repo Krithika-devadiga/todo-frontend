f21214e795cc16cfc32250246a254f48c0863d4b
Krithika-devadiga <krithikadevadiga13@gmail.com>
2025-06-26 21:28:17 +0530

    save changes

--- src/App.js
+++ src/App.js
@@ -1,52 +1,73 @@
 import React, { useEffect, useState } from 'react';
+import api from './api';
 import TodoForm from './components/TodoForm';
 import TodoList from './components/TodoList';
-import api from './api';
 import './App.css';
 
 function App() {
- const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState([]);
+  const [title, setTitle] = useState('');
 
- const fetchTodos = () => {
-   api.get('/').then(res => setTodos(res.data));
- };
+  const fetchTodos = () => {
+    api.get('/').then(res => setTodos(res.data));
+  };
 
- const deleteTodo = async (id) => {
-   await api.delete(`/${id}`);
-   fetchTodos();
- };
+  const addTodo = async () => {
+    if (!title.trim()) return;
+    await api.post('/', { title });
+    setTitle('');
+    fetchTodos();
+  };
 
- const toggleCompleted = async (id, completed) => {
-   await api.put(`/${id}`, { completed });
-   fetchTodos();
- };
+  const deleteTodo = async (id) => {
+    await api.delete(`/${id}`);
+    fetchTodos();
+  };
 
- useEffect(() => {
-   fetchTodos();
- }, []);
+  const toggleCompleted = async (id, completed) => {
+    await api.put(`/${id}`, { completed });
+    fetchTodos();
+  };
 
- return (
-   <ul>
-      {todos.map((todo) => (
-        <li key={todo._id}>
-          <div className="todo-item">
-            <span
-              style={{
-                textDecoration: todo.completed ? 'line-through' : 'none',
-                cursor: 'pointer'
-              }}
-              onClick={() => onToggle(todo._id, !todo.completed)}
-            >
-              {todo.title}
-            </span>
-            <button className="delete-btn" onClick={() => onDelete(todo._id)}>
-              Delete
-            </button>
-          </div>
-            </li>
-      ))}
-    </ul>
- );
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+
+  return (
+    <div className="container">
+      <h1>To-Do App</h1>
+      <div>
+        <input
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          placeholder="Add a task"
+        />
+        <button onClick={addTodo}>Add</button>
+      </div>
+
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo._id}>
+            <div className="todo-item">
+              <span
+                onClick={() => toggleCompleted(todo._id, !todo.completed)}
+                style={{
+                  textDecoration: todo.completed ? 'line-through' : 'none',
+                  cursor: 'pointer',
+                  flex: 1
+                }}
+              >
+                {todo.title}
+              </span>
+              <button className="delete-btn" onClick={() => deleteTodo(todo._id)}>
+                Delete
+              </button>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
